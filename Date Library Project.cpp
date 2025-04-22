ae811c4df1a5da078e0fee79bba0dd8edcbf20a7
Mohamed Hassan <mohamed1126983220@gmail.com>
2025-04-22 10:35:40 +0200

    Add files via upload
    This is Main you must insure that there is " #include"clsDate.h" " and " #include "clsString.h" "

---
+++ Date Library Project.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<cctype>
+#include<fstream>
+#include<cstdlib>
+#include"clsDate.h";
+#include "clsString.h";
+
+using namespace std;
+
+int main()
+{
+
+	system("pause>0");
+
+
+}
